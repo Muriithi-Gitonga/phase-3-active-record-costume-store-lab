a5e8afbbfa6403b2aa8941da8459e3e278332486
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-11-04 09:34:11 +0300

    done

--- db/migrate/20221103221713_costumes.rb
+++ db/migrate/20221103221713_costumes.rb
@@ -1,6 +1,6 @@
 class Costumes < ActiveRecord::Migration[6.1]
   def change
-    create_table :Costumes do |t|
+    create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :size
